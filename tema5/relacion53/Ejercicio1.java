6d820e4eec607f6e5f528414cbb114aa2c83b19b
adriifdez <fdezadri22@gmail.com>
2025-12-12 10:36:28 +0100

    Cambio Ejercicio1 de la relacion53 (#1)
    esta todo correcto

--- tema5/relacion53/Ejercicio1.java
+++ tema5/relacion53/Ejercicio1.java
@@ -3,6 +3,8 @@ package tema5.relacion53;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
+		System.out.println("Este ha sido mi cambio realizado");
+
 		double[][][] claseDaw = new double[20][6][10];
 		
 		for(int alumno=0;alumno<claseDaw[20].length;alumno++) {
